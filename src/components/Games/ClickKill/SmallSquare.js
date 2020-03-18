615b85647134d98ac3fc3c29024b6c84792b6735
Julie Brosse <privatejuliedev@gmail.com>
2020-03-18 11:31:44 +0100

    testing but not working yet

--- src/components/Games/ClickKill/SmallSquare.js
+++ src/components/Games/ClickKill/SmallSquare.js
@@ -12,12 +12,36 @@ const SmallSquare = props =>(
     props.show
     // COMMENT writing the z-index into the id of the div below
         ? <CardMedia
-            className={props.logoClassName}
+            className="bla"
             image={props.logo}
             title="team logo"
             onClick={props.itemClicked}
       />
         : null
 )
+
+// const SmallSquare = props => {
+//     // SmallSquare.state.display = false ? return counter : return counter + 1 and it changes back to false
+//     if (props.show && props.logoClicked) {
+//         return (
+//     // COMMENT writing the z-index into the id of the div below
+//         <CardMedia
+//             className="logoDisplayed"
+//             image={props.logo}
+//             title="team logo"
+//             onClick={props.itemClicked}
+//         />)
+//       } else if (props.show) {
+//           return (
+//         <CardMedia
+//         className="logoHidden"
+//         image={props.logo}
+//         title="team logo"
+//         onClick={props.itemClicked}
+//     />)
+//       } else {
+//         return null
+//     }
+// }
 
  export default SmallSquare;
