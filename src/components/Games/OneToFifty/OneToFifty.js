deb3f46b9d96886c13fc745d37bb99a05dd9ba2d
MariaRomanDelonia <maria.roman@delonia.es>
2020-04-01 16:31:23 +0200

    OneToFifty model

--- src/components/Games/OneToFifty/OneToFifty.js
+++ src/components/Games/OneToFifty/OneToFifty.js
@@ -1,33 +1,88 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './OneToFifty.scss';
-import { Button } from 'reactstrap';
+import {
+    Button,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Modal, ModalBody, ModalFooter
+} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function OneToFifty() {
+    //Instructions Icon
+    const instructions = <FontAwesomeIcon icon={faQuestionCircle} />
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
 
     // ---STATES---
-    const [numbersList, changenumbersList] = useState({
-        numbers: []
-    });
+    // let [numbersList, changenumbersList] = useState([]);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const toggleDropdown = () => setDropdownOpen(prevState => !prevState);
+    const [numberCells, changeNumberCells] = useState(12);
+    let [seconds, changeSeconds] = useState(0);
     // ---END OF STATES---
-    let gridSize = 12;
-    let cell=[];
-    for (let i = 0; i < gridSize; i++) {
+
+    //Counter time
+    setTimeout(() => {
+        changeSeconds(seconds + 1);
+    }, 1000);
+
+    //Grid size for diferents levels
+    let cell = [];
+    for (let i = 0; i < numberCells; i++) {
         cell.push(<div className="col-3" key={i}><div className="cellNumber alignCenter justifyCenter">hola</div></div>)
     }
+
     return (
         <div className="OneToFifty container-fluid">
+            {/* INSTRUCTIONS */}
+            <div className="row">
+                <span style={{ color: "#283f67", fontSize: "2em", marginLeft: "90%" }} onClick={toggle}>{instructions}</span>
+                <Modal isOpen={modal} toggle={toggle} style={{ marginTop:"27%"}}>
+                    <ModalBody>
+                        <p>Cuenta del 1 al 50 lo más rápido posible...</p>
+                        <p>¡Consigue tu tiempo récord!</p>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={toggle}>Cerrar</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+            {/* MENU */}
             <div className="row timeAndActualNumber alignCenter">
-                <div className="col-6">
-                    <p>Tiempo</p>
+                <div className="col-12 col-md-6">
+                    <div className="row justifyCenter">
+                        <p>Tiempo: {seconds}</p>
+                    </div>
+                    <div className="row justifyCenter">
+                        <p>Numero actual: X</p>
+                    </div>
                 </div>
-                <div className="col-6">
-                    <p>Numero actual</p>
+                <div className="col-12 col-md-6 justifyCenter">
+                    <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
+                        <DropdownToggle caret className="selector_button button" >
+                            Selecciona nivel
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem onClick={() => changeNumberCells(4)}>Aprende</DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem onClick={() => changeNumberCells(8)}>Fácil</DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem onClick={() => changeNumberCells(12)}>Medio</DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem onClick={() => changeNumberCells(16)}>Difícil</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
                 </div>
             </div>
+            {/* GAME */}
             <div className="row numbersGrid">
                 {cell}
             </div>
-            <Button id="button">Restart</Button>
+            <Button className="button" onClick={() => window.location.reload()}>Restart</Button>
         </div>
     )
 }
