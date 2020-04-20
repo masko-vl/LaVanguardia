33ee923859793fd1e7b8e924d3650827e3751fce
MariaRomanDelonia <maria.roman@delonia.es>
2020-04-20 19:19:31 +0200

    chrono fixed

--- src/components/Games/OneToFifty/OneToFifty.js
+++ src/components/Games/OneToFifty/OneToFifty.js
@@ -54,7 +54,7 @@ export default function OneToFifty() {
             <div className="row timeAndActualNumber alignCenter">
                 <div className="col-12 col-md-6">
                     <div className="row justifyCenter">
-                        <Chrono/>
+                        {timeStart === false? '00:00:00' : <Chrono/>} 
                     </div>
                     <div className="row justifyCenter">
                         <p>Tienes que buscar el numero: {currentNumber}</p>
