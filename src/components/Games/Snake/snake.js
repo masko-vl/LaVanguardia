5c0de1ccafef8095a6c8c40317121027cd819847
Julie Brosse <privatejuliedev@gmail.com>
2020-04-22 12:47:26 +0200

    snake fixed on last version

--- src/components/Games/Snake/snake.js
+++ src/components/Games/Snake/snake.js
@@ -4,13 +4,12 @@ export default (props) => {
   return (
     <div>
       {props.snakeDots.map((dot, i) => {
-
         const style = {
           left: `${dot[0]}%`,
           top: `${dot[1]}%`
         }
         return (
-          <div className="snake-dot" id={`snake-dot-${i}`} key={i} style={style}></div>
+          <div className="snake-dot" key={i} style={style}></div>
         )
       })}
     </div>
