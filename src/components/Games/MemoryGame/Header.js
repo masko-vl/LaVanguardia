f162df9e3cadbf597167989f13c4d444c11b0308
Julie Brosse <privatejuliedev@gmail.com>
2020-04-24 17:47:41 +0200

    icons and everything responsive ok

--- src/components/Games/MemoryGame/Header.js
+++ src/components/Games/MemoryGame/Header.js
@@ -5,18 +5,21 @@ class Header extends React.Component {
     render() {
         return(
             <header className="memory-header">
-                <h1 className="title">
+                <div className="title">
                     Memory de presidentes
-                </h1>
+                </div>
 
                 <div>
                 {this.props.gameFinished 
                     ? 
                         <div>
                         {/* {` Resultado: ${ Math.round(10 / this.props.tryes * 10) }/ 10 puntos !`} */}
-                        <button className="winnerMemoryResult">{` Resultado: ${ Math.round(10 / this.props.tryes * 10) }/ 10 puntos`}</button>
+                        <button className="winnerMemoryResult">` Resultado: 13/ 10 puntos`</button>
                         <button className="winner-reinit-button" onClick={this.props.resetGame}>
                             INTÉNTALO OTRA VEZ !
+                        </button>
+                        <button className="winner-reinit-button-iframe" onClick={this.props.resetGame}>
+                        <i class="fas fa-undo-alt"></i>
                         </button>
                         </div>
                         
