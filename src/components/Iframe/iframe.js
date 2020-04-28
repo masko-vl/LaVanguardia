26d53dd2033566bccff4413d6850ea3de41e035e
mireiaGomariz <mireiapgomariz@gmail.com>
2020-04-28 07:48:58 +0200

    iframe test page full

--- src/components/Iframe/iframe.js
+++ src/components/Iframe/iframe.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import CloseButton from '../SharedButtons/CloseButton';
 import '../HomePage/buttonGames.css'
 import './iframe.css';
 
@@ -13,15 +14,78 @@ const displayRandomItem = () => {
 
 const Iframe = () => {
   return (
-  <div>
-    <iframe src={"//localhost:3000/snake"}
-        width="330px"
-        height="260px"
-        id="test"
-        className="myClassname"
-        display="initial"
-        position="relative"
-        />
+  <div className="allIframesContainer">
+    <CloseButton />
+    <div>
+      <h3 className="titleIframeAll">Snake</h3>
+      <iframe src={"//localhost:3000/snake"}
+          width="330px"
+          height="260px"
+          id="test"
+          className="myClassname"
+          display="initial"
+          position="relative"
+          />
+    </div>
+    <div>
+      <h3 className="titleIframeAll">Tacle Click</h3>
+      <iframe src={"//localhost:3000/tacleclick"}
+          width="330px"
+          height="260px"
+          id="test"
+          className="myClassname"
+          display="initial"
+          position="relative"
+          />
+    </div>
+
+    <div>
+      <h3 className="titleIframeAll">Memory de Presidentes</h3>
+      <iframe src={"//localhost:3000/MemoryGame"}
+          width="330px"
+          height="260px"
+          id="test"
+          className="myClassname"
+          display="initial"
+          position="relative"
+          />
+    </div>
+
+    <div>
+      <h3 className="titleIframeAll">Nonogram</h3>
+      <iframe src={"//localhost:3000/nonogram"}
+          width="330px"
+          height="260px"
+          id="test"
+          className="myClassname"
+          display="initial"
+          position="relative"
+          />
+    </div>
+
+    <div>
+      <h3 className="titleIframeAll">City Play</h3>
+      <iframe src={"//localhost:3000/cityplay"}
+          width="330px"
+          height="260px"
+          id="test"
+          className="myClassname"
+          display="initial"
+          position="relative"
+          />
+    </div>
+
+    <div>
+      <h3 className="titleIframeAll">1 to 50</h3>
+      <iframe src={"//localhost:3000/OneToFifty"}
+          width="330px"
+          height="260px"
+          id="test"
+          className="myClassname"
+          display="initial"
+          position="relative"
+          />
+    </div>
   </div>
   )
 }
