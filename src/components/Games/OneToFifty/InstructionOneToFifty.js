b25be850c587012926c3a8de7db20128074bd127
jciria74 <47560520+jciria74@users.noreply.github.com>
2020-04-28 19:37:50 +0200

    nonogram and OneToFifty instructions

--- src/components/Games/OneToFifty/InstructionOneToFifty.js
+++ src/components/Games/OneToFifty/InstructionOneToFifty.js
@@ -21,18 +21,25 @@ export class InstructionOneToFifty extends React.Component {
    
   render(){
        //Instructions Icon
-       const instructions = < FontAwesomeIcon icon = {
+       const instructions = <FontAwesomeIcon icon = {
            faQuestionCircle
        }
        />
 
       return (
-        < div>
+        <div>
             {/* INSTRUCTIONS */}
                 <span className="bigInstructions" style={{ color: "rgb(245, 123, 75)", fontSize: "2em"}} onClick={this.toggle}>{instructions}</span>
                 <Modal isOpen={this.state.modal} toggle={this.toggle} style={{ marginTop:"10%"}}>
                     <ModalBody>
-                        <div><p>¡Intenta contar de uno a cincuenta lo más rapido posible! ¿Eres más rápido que tus amigos?</p></div>
+                        <div><p>¡Intenta contar de uno a cincuenta lo más rapido posible! ¿Eres más rápido que tus amigos?<br/><br/>
+                        Más de 100 segundos: ve a por un café y ya si eso ....<br/>
+                        60-100 segundos: estás en la media<br/>
+                        50-60 segundos: por encima de la media<br/>
+                        40-50 segundos: nivel EXPERTO<br/>
+                        30-40 segundos: nivel DIOS<br/>
+                        Menos de 30 segundos: ... mentiroso?<br/>
+                        </p></div>
                     </ModalBody>
                     <ModalFooter>
                         <Button color="primary" onClick={this.toggle}>Cerrar</Button>
